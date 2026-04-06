eb897ccaaee1f637c5e081013f9ced40b2f9e4d8
Eric Reilly <ericreilly999@gmail.com>
2026-04-06 12:44:25 -0400

    fix: align terraform attributes in http_api module
    Align attributes in all resource blocks in the http_api module to match
    terraform fmt requirements.

--- terraform/modules/http_api/main.tf
+++ terraform/modules/http_api/main.tf
@@ -1,6 +1,6 @@
 resource "aws_apigatewayv2_api" "this" {
-  name          = var.name
-  protocol_type = "HTTP"
+  name           = var.name
+  protocol_type  = "HTTP"
 
   cors_configuration {
     allow_headers = ["authorization", "content-type"]
@@ -9,15 +9,15 @@ resource "aws_apigatewayv2_api" "this" {
     max_age       = 3600
   }
 
-  tags = var.tags
+  tags           = var.tags
 }
 
 resource "aws_apigatewayv2_authorizer" "jwt" {
-  count            = var.authorizer_issuer == "" ? 0 : 1
-  api_id           = aws_apigatewayv2_api.this.id
-  name             = "${var.name}-jwt"
-  authorizer_type  = "JWT"
-  identity_sources = ["$request.header.Authorization"]
+  count             = var.authorizer_issuer == "" ? 0 : 1
+  api_id            = aws_apigatewayv2_api.this.id
+  name              = "${var.name}-jwt"
+  authorizer_type   = "JWT"
+  identity_sources  = ["$request.header.Authorization"]
 
   jwt_configuration {
     audience = var.authorizer_audience
@@ -26,41 +26,41 @@ resource "aws_apigatewayv2_authorizer" "jwt" {
 }
 
 resource "aws_apigatewayv2_integration" "lambda" {
-  api_id                 = aws_apigatewayv2_api.this.id
-  integration_type       = "AWS_PROXY"
-  integration_method     = "POST"
-  integration_uri        = var.integration_uri
-  payload_format_version = "2.0"
+  api_id                   = aws_apigatewayv2_api.this.id
+  integration_type         = "AWS_PROXY"
+  integration_method       = "POST"
+  integration_uri          = var.integration_uri
+  payload_format_version   = "2.0"
 }
 
 resource "aws_apigatewayv2_route" "this" {
   for_each = var.routes
 
-  api_id    = aws_apigatewayv2_api.this.id
-  route_key = each.value
-  target    = "integrations/${aws_apigatewayv2_integration.lambda.id}"
+  api_id              = aws_apigatewayv2_api.this.id
+  route_key           = each.value
+  target              = "integrations/${aws_apigatewayv2_integration.lambda.id}"
 
-  authorization_type = contains(tolist(var.protected_routes), each.value) && length(aws_apigatewayv2_authorizer.jwt) > 0 ? "JWT" : "NONE"
-  authorizer_id      = contains(tolist(var.protected_routes), each.value) && length(aws_apigatewayv2_authorizer.jwt) > 0 ? aws_apigatewayv2_authorizer.jwt[0].id : null
+  authorization_type  = contains(tolist(var.protected_routes), each.value) && length(aws_apigatewayv2_authorizer.jwt) > 0 ? "JWT" : "NONE"
+  authorizer_id       = contains(tolist(var.protected_routes), each.value) && length(aws_apigatewayv2_authorizer.jwt) > 0 ? aws_apigatewayv2_authorizer.jwt[0].id : null
 }
 
 resource "aws_apigatewayv2_stage" "this" {
-  api_id      = aws_apigatewayv2_api.this.id
-  name        = var.stage_name
-  auto_deploy = true
+  api_id       = aws_apigatewayv2_api.this.id
+  name         = var.stage_name
+  auto_deploy  = true
 
   default_route_settings {
     throttling_burst_limit = 20
     throttling_rate_limit  = 10
   }
 
-  tags = var.tags
+  tags         = var.tags
 }
 
 resource "aws_lambda_permission" "api_invoke" {
-  statement_id  = "AllowExecutionFromHttpApi"
-  action        = "lambda:InvokeFunction"
-  function_name = var.lambda_function_name
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_apigatewayv2_api.this.execution_arn}/*/*"
+  statement_id   = "AllowExecutionFromHttpApi"
+  action         = "lambda:InvokeFunction"
+  function_name  = var.lambda_function_name
+  principal      = "apigateway.amazonaws.com"
+  source_arn     = "${aws_apigatewayv2_api.this.execution_arn}/*/*"
 }
